9c35f9f14a080a6092e3ad2d0d490dc01193a359
lxllxl233 <2209706070@qq.com>
2025-03-01 17:43:33 +0800

    初始化常用钩子

--- conftest.py
+++ conftest.py
@@ -48,3 +48,27 @@ def pytest_runtest_logfinish(nodeid, location):
             logger.info(info['case_name'], suffer='case_end')
         else:
             logger.info(nodeid, suffer='case_end')
+
+# 用例收集钩子
+def pytest_collection_modifyitems(items):
+    print(items)
+    print(f"已收集 {len(items)} 个测试用例")
+
+# 用例过滤完成钩子，这里是实际执行用例的地方
+def pytest_collection_finish(session):
+    # 获取所有用例信息
+    selected = session.items
+    print(f"已选中执行 {len(selected)} 个测试用例")
+    print('初始化自定义组件')
+    ######## 这里放置自己的组件 ########
+    # 放置组件 : FrameBean.set_bean('组件名', 组件对象)
+    # 获取组件 : FrameBean.get_service('组件名')
+    print('初始化自定义组件完成')
+
+# 用例执行完成钩子
+def pytest_sessionfinish(session, exitstatus):
+    if exitstatus == 0:
+        print("✅ 所有测试通过")
+    else:
+        print("❌ 存在失败的用例")
+
